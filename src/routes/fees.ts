d8692d92653654c0c4b1dd321ddee36ed13144c4
Pranay Baldev <pranaybaldev@gmail.com>
2022-02-21 17:06:39 +0530

    calculate fee

--- src/routes/fees.ts
+++ src/routes/fees.ts
@@ -1,19 +1,22 @@
 import { FastifyInstance } from "fastify/types/instance";
-import { FeesResponse,FeesRequest   } from "@emeris/types/lib/EmerisFees"
+import { FeesResponse, FeesRequest } from "@emeris/types/lib/EmerisFees"
 import EmerisSigner from '@emeris/signer';
 import emerisMapper from '@emeris/mapper';
 import { AminoMsg } from "@cosmjs/amino";
 import ChainConfig from "../chainConfig";
+import FeeService from '../services/fees.service'
 
 function add(server: FastifyInstance) {
 	server.post<{ Body: FeesRequest; Reply: FeesResponse }>('/fees', {
 		async handler(request, reply) {
 			const config = new ChainConfig('https://api.emeris.com/v1')
 			const chain = await config.getChain(request.body.tx.chainName);
+			//TODO: remove mnemonic
 			const signer = EmerisSigner.withMnemonic(chain.derivation_path, ''); // MUST ADD MNEMONIC
 			const mapped = await emerisMapper(request.body.tx);
-			const fees = await signer.getFees({ chain_name: request.body.tx.chainName, msgs: mapped as AminoMsg[], memo: '' });			
-			reply.send(fees as any)
+			const fees = await signer.getFees({ chain_name: request.body.tx.chainName, msgs: mapped as AminoMsg[], memo: '' });		
+			const { fee, totalUSD } = await FeeService.calculateFees({gasUsed: (fees as any).GasUsed, gasPriceLevel: request?.body?.gasPriceLevel, chain})
+			reply.send({gasUsed: (fees as any).GasUsed, fee: fee, totalUSD: totalUSD} as any)
 		}, 
 		schema: {
 			body: FeesRequest,
---
+++ src/services/fees.service.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import BigNumber from "bignumber.js";
+
+export default class FeeService {
+    static async calculateFees({gasUsed, gasPriceLevel, chain}){
+        try{
+            const inputGasPriceLevel = gasPriceLevel ? gasPriceLevel : 'average';
+            const denom = chain?.denoms?.find((denom) => !!denom.fee_token);
+            const feeAmount = new BigNumber(gasUsed).multipliedBy((denom as any).gas_price_levels[inputGasPriceLevel]).toNumber();
+            const prices = await axios.get('https://api.emeris.com/v1/oracle/prices');
+            const unitPriceTokenUSD = prices.data.data.Tokens.find((token) => token?.Symbol === `${(denom as any).ticker}USDT`)?.Price
+            const totalUSD = new BigNumber(feeAmount).multipliedBy(unitPriceTokenUSD).shiftedBy(-denom.precision).decimalPlaces(denom.precision).toNumber();
+            const fee = [{amount: feeAmount, denom: (denom as any).name}]
+            return { fee, totalUSD }
+        } catch (e){
+            throw new Error("Could not calculate fee: "+ e);
+        }
+        
+    }
+}
