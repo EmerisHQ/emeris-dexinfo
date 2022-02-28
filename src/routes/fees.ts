5a9766af03bec40bc55892bbbb05cc3b6f5e6137
Pranay Baldev <pranaybaldev@gmail.com>
2022-02-28 17:54:23 +0530

    fee req res to array

--- src/routes/fees.ts
+++ src/routes/fees.ts
@@ -9,14 +9,18 @@ import FeeService from '../services/fees.service'
 function add(server: FastifyInstance) {
 	server.post<{ Body: FeesRequest; Reply: FeesResponse }>('/fees', {
 		async handler(request, reply) {
-			const config = new ChainConfig('https://api.emeris.com/v1')
-			const chain = await config.getChain(request.body.tx.chainName);
-			//TODO: remove mnemonic
-			const signer = EmerisSigner.withMnemonic(chain.derivation_path, ''); // MUST ADD MNEMONIC
-			const mapped = await emerisMapper(request.body.tx);
-			const fees = await signer.getFees({ chain_name: request.body.tx.chainName, msgs: mapped as AminoMsg[], memo: '' });		
-			const { fee, totalUSD } = await FeeService.calculateFees({gasUsed: (fees as any).GasUsed, gasPriceLevel: request?.body?.gasPriceLevel, chain})
-			reply.send({gasUsed: (fees as any).GasUsed, fee: fee, totalUSD: totalUSD} as any)
+			const response = [];
+			for (const feesRequest of request.body){
+				const config = new ChainConfig('https://api.emeris.com/v1')
+				const chain = await config.getChain(feesRequest.tx.chainName);
+				//TODO: remove mnemonic
+				const signer = EmerisSigner.withMnemonic(chain.derivation_path, ''); // MUST ADD MNEMONIC
+				const mapped = await emerisMapper(feesRequest.tx);
+				const fees = await signer.getFees({ chain_name: feesRequest.tx.chainName, msgs: mapped as AminoMsg[], memo: '' });		
+				const { fee, totalUSD } = await FeeService.calculateFees({gasUsed: (fees as any).GasUsed, gasPriceLevel: feesRequest.gasPriceLevel, chain})
+				response.push({gasUsed: (fees as any).GasUsed, fee: fee, totalUSD: totalUSD})	
+			}
+			reply.send(response)
 		}, 
 		schema: {
 			body: FeesRequest,
